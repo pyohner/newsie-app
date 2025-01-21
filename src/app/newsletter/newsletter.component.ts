4d76b5391d161520510ed8ea7a15f468f6f3d531
Phillip Yohner <phillip.n.yohner@disney.com>
2025-01-21 13:12:36 -0500

    Update 4

---
+++ src/app/newsletter/newsletter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletter',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      newsletter works!
+    </p>
+  `,
+  styleUrl: './newsletter.component.css'
+})
+export class NewsletterComponent {
+
+}
--- src/app/newsstand/newsstand.component.ts
+++ src/app/newsstand/newsstand.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { NewsletterComponent} from "../newsletter/newsletter.component";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-newsstand',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NewsletterComponent
+  ],
   template: `
     <section>
       <form>
         <input type="text" placeholder="Filter by summary">
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-newsletter></app-newsletter>
     </section>
   `,
   styleUrl: './newsstand.component.css'
