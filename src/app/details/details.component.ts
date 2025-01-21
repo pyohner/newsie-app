807ac74a39931d10c728656e063c366c824c2120
Phillip Yohner <phillip.n.yohner@disney.com>
2025-01-21 15:13:01 -0500

    Update 11

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { NewsService} from "../news.service";
+import { Newsletter} from "../newsletter";
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      details works!
-    </p>
+    <article>
+      <img class="listing-photo" [src]="newsletter?.photo"
+           alt="Image of {{newsletter?.name}}"/>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{newsletter?.name}}</h2>
+        <p class="listing-location">{{newsletter?.summary}}, {{newsletter?.category}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this newsletter</h2>
+        <ul>
+          <li>Frequency: {{newsletter?.frequency}}</li>
+          <li>Description: {{newsletter?.description}}</li>
+        </ul>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  newsService = inject(NewsService);
+  newsletter: Newsletter | undefined;
 
+  constructor() {
+    const newsletterId = Number(this.route.snapshot.params['id']);
+    this.newsletter = this.newsService.getNewsletterById(newsletterId);
+  }
 }
--- src/app/newsletter/newsletter.component.ts
+++ src/app/newsletter/newsletter.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import {Newsletter} from "../newsletter";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-newsletter',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
   template: `
     <section class="listing">
       <img class="listing-photo" [src]="newsletter.photo" alt="Exterior photo of {{newsletter.name}}">
       <h2 class="listing-heading">{{ newsletter.name }}</h2>
       <p class="listing-location">{{ newsletter.summary}}<br>Category: {{newsletter.category }}</p>
+      <a [routerLink]="['/details', newsletter.id]">Learn More</a>
     </section>
   `,
   styleUrl: './newsletter.component.css'
