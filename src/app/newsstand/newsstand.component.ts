63bcfd6d25752c5729e4afc9d6c49b9987fde12e
Phillip Yohner <phillip.n.yohner@disney.com>
2025-01-21 14:11:00 -0500

    Update 8

--- src/app/newsstand/newsstand.component.ts
+++ src/app/newsstand/newsstand.component.ts
@@ -18,7 +18,10 @@ import {Newsletter} from "../newsletter";
       </form>
     </section>
     <section class="results">
-      <app-newsletter [newsletter]="newsletter"></app-newsletter>
+      <app-newsletter
+        *ngFor="let newsletter of newsletterList"
+        [newsletter]="newsletter">
+      </app-newsletter>
     </section>
   `,
   styleUrl: './newsstand.component.css'
@@ -26,13 +29,96 @@ import {Newsletter} from "../newsletter";
 export class NewsstandComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  newsletter: Newsletter = {
-    id: 9999,
-    name: 'Test Home',
-    summary: 'Test city',
-    category: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    frequency: 'daily',
-    description: 'something newsie!',
-  };
+  newsletterList: Newsletter[] = [
+    {
+      id: 0,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 1,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 2,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 3,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 4,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 5,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 6,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 7,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 8,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    },
+    {
+      id: 9,
+      name: 'Test Home',
+      summary: 'Test city',
+      category: 'ST',
+      photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
+      frequency: 'daily',
+      description: 'something newsie!',
+    }
+  ];
 }
